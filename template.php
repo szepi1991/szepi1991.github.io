913697ac012fa02a352a9ceedaea83f28db8c4a5
sud03r <17.neeraj.s@gmail.com>
2012-09-17 01:14:30 +0530

    Finally Designed the homepage!

--- template.php
+++ template.php
@@ -1,36 +1,38 @@
+<?php require_once 'bootstrap.php' ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <link href="css/base.css" type="text/css" rel="stylesheet"/>
         <script type="text/javascript" src="js/jquery.js"></script>
         <script type="text/javascript" src="js/jquery.validate.js"></script>
-        <title> Welcome!</title>
+        <title> Your title here</title>
     </head>
 
     <body>
         <div id='header' class='centered content'>
-            Neeraj Kumar
+            <img id='sf-logo' src='images/logo.png'/>
             <ul id='sf-link-container'>
-                <li class='round-left-bottom-corner'><a href='#'>About Me</a></li>
-                <li><a href='#'>Projects</a></li>
-                <li><a href='#'>Resume</a></li>
-                <li class='round-right-bottom-corner'><a href='#'>Research</a></li>
+                <li class='round-left-bottom-corner'><a href='#'>link 1</a></li>
+                <li class='round-right-bottom-corner'><a href='#'>link2</a></li>
             </ul>
         </div>
         <div id='page' class='centered content'>
             This is a template file. It just defines the layout.<br/>
-            Don't edit it. Make a copy and then make your changes. <br/>
-
+            Don't edit it. Make a copy and then make your changes.
         </div>
         <div id='footer'>
             <div class='centered content'> 
-                <span class='left'>Copyright &copy; 2012</span>
+                <span class='left'>Copyright &copy; 2012 ShareFare.com</span>
                 <span class='right'> 
                     <ul id='footer-link-container'>
-                        <li><a href="http://stackoverflow.com/users/91593/sud03r">
-                        <img src="http://stackoverflow.com/users/flair/91593.png" width="208" height="58" 
-                            title="Profile of sud03r at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
-                        </a></li>
+                        <li><a href='#'>About Us</a></li>
+                        <li><a href='#'>FAQs</a></li>
+                        <li><a href='#'>Terms and Conditions</a></li>
+                        <li><a href='#'>Careers</a></li>
+                        <li><a href='#'>Contact Us</a></li>
+                        <li><img src='images/f.png'/></li>
+                        <li><img src='images/g.jpeg'/></li>
+                        <li><img src='images/t.jpg'/></li>
                     </ul>
                 </span>
             </div>
